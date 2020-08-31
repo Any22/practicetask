bc8f3fc63ceb163fdae45d3758a7e067629c8fd7
Saba Akhtar <sabasaeedakhtar@gmail.com>
2020-08-31 23:56:45 +1000

    remove pic from add and update function

--- taskmanager.js
+++ taskmanager.js
@@ -5,12 +5,12 @@ export default class CardManager{
     this.currentId = 1;
     this.master =master;
     }
-        addcard(cname,pic,description,assignee,dDate,st){
-                let nCard=new Card(`nCard${this.currentId++}`,cname,pic,description,assignee,dDate,st); //creates an instance of class card
+        addcard(cname,description,assignee,dDate,st){
+                let nCard=new Card(`nCard${this.currentId++}`,cname,description,assignee,dDate,st); //creates an instance of class card
                 this.cardArr.push(nCard);                                                               //pushing an items in form of cards in card array
             }
 
-        updateTask(id,cname,pic,description,assignee,dDate,st) {                                        //Function to update TASK
+        updateTask(id,cname,description,assignee,dDate,st) {                                        //Function to update TASK
                 // alert("I am in update function");
                 // alert(id);
                 for (let i=0; i<this.cardArr.length;i++) {
@@ -40,16 +40,21 @@ export default class CardManager{
         displayListHtml(edifunc,delfunc){
               
                 // alert("I am in display");
-                
-                // alert(this.cardArr.nCard);
                 this.master.innerHTML ="";
-                let cardhtml;
-                this.cardArr.forEach((nCard) => {
-                    // alert(this.cardArr[0].description);
-                let taskElement = nCard.toElement(edifunc,delfunc);
-                this.master.append(taskElement);
-            
-                  });
-                }
+                    for (let i=0 ; i< this.cardArr.length ; i++){
+                        const nCard = new Card (this.cardArr[i].id,
+                            this.cardArr[i].cname,
+                            this.cardArr[i].description,
+                            this.cardArr[i].assignee,
+                            this.cardArr[i].dDate,
+                            this.cardArr[i].st
+                        );
+                    let taskElement = nCard.toElement(edifunc,delfunc);
+                    this.master.append(taskElement);
+                    }
+               
+            }
                 
-}                         
+ 
+        }
+                     
