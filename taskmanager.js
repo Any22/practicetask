d7ac42fac000d7872932e56e509bf83baba57d52
Saba Akhtar <sabasaeedakhtar@gmail.com>
2020-10-01 15:52:44 +1000

    fixed id issues

--- taskmanager.js
+++ taskmanager.js
@@ -1,110 +1,89 @@
 import Card from "./task.js"
 export default class CardManager{
     constructor(master) {
-    this.cardArr=[];
-    this.currentId = 1;
+    this.cardArr= JSON.parse(localStorage.getItem("mytasks")) || [];
+    this.currentId = parseInt(localStorage.getItem('currentID'))|| 1;
+    localStorage.setItem('currentID',this.currentId);
     this.master =master;
     }
         addcard(cname,description,assignee,dDate,st){
-                let nCard=new Card(`nCard${this.currentId++}`,cname,description,assignee,dDate,st); //creates an instance of class card
-                this.cardArr.push(nCard);                                                               //pushing an items in form of cards in card array
-                this.sendLocalStorage(nCard);
-        }
-        sendLocalStorage(nCard){
-                localStorage.setItem('currentId',this.currentId);
-                let mynewtasks = JSON.parse(localStorage.getItem('mytasks')) || [];
+                const nCard=new Card(`nCard${this.currentId++}`,cname,description,assignee,dDate,st); //creates an instance of class card
+                localStorage.setItem('currentID',this.currentId);
+                let mynewtasks = JSON.parse(localStorage.getItem("mytasks")) || [];
                 mynewtasks.push(nCard);
-                localStorage.setItem('mytasks',JSON.stringify(mynewtasks));
-            }
-
+                localStorage.setItem("mytasks",JSON.stringify(mynewtasks));
+                this.cardArr.push(nCard);                                                               //pushing an items in form of cards in card array
+                
+        }
+        
         updateTask(id,cname,description,assignee,dDate,st) {                                        //Function to update TASK
-                // alert("I am in update function");
-                // alert(id);
-                for (let i=0; i<this.cardArr.length;i++) {
-                    
-                    if(this.cardArr[i].id == id) {
-                    // alert(" im in update condition");
-                    this.cardArr[i].cname = cname;
-                    this.cardArr[i].description = description;
-                    this.cardArr[i].assignee = assignee; 
-                    this.cardArr[i].dDate = dDate;
-                    this.cardArr[i].st = st;
-                    this.sendUpdateLstorage (id,cname,description,assignee,dDate,st);
-                    }
-                }
-            }
-            sendUpdateLstorage (id,cname,description,assignee,dDate,st)
-            {
-                    let mynewtasks= JSON.parse(localStorage.getItem("mytasks"));
-                    for (let i=0; i<mynewtasks.length;i++) {
-                        if(mynewtasks[i].id == id) {
+            let mynewtasks=JSON.parse(localStorage.getItem("mytasks"));
+                for (let i=0; i<mynewtasks.length;i++) {
+                    if(mynewtasks[i].id === id) {
                         mynewtasks[i].cname = cname;
                         mynewtasks[i].description = description;
                         mynewtasks[i].assignee = assignee; 
                         mynewtasks[i].dDate = dDate;
-                        mynewtasks[i].st = st;
+                        mynewtasks[i].st = st; 
                         localStorage.setItem("mytasks",JSON.stringify(mynewtasks));
                 
-                    }
+                        this.cardArr[i].id == id;
+                   
+                        this.cardArr[i].cname = cname;
+                        this.cardArr[i].description = description;
+                        this.cardArr[i].assignee = assignee; 
+                        this.cardArr[i].dDate = dDate;
+                        this.cardArr[i].st = st;
+                       
+                        }
                 }
             }
+           
         deletFunc(id)
         {
-                for (let i=0 ; i<this.cardArr.length; i++)
-                    {
-                        if (this.cardArr[i].id == id ){
-                        this.cardArr.splice(i,1);                               //An array (Array) containing the deleted elements.
-                        this.delLocalstorage(id);
-                    }
-                }
-            }           delLocalstorage(id){
-                                                        
-                        let mynewtasks= JSON.parse(localStorage.getItem("mytasks"));
-                        for (let i=0 ; i<mynewtasks.length; i++)
-                        {
-                        if (mynewtasks[i].id == id ){
-                        mynewtasks.splice(i,1); 
-                        localStorage.setItem('mytasks',JSON.stringify(mynewtasks));      
-                        }
-                        
-                    }
-                 }
+            this.cardArr=this.cardArr.filter(m => m.id !== id);                       // filtering all tasks other than selected id which is passed throught function
+            let mynewtasks=JSON.parse(localStorage.getItem("mytasks"));               // retrieving/reading tasks from localstorage 
+            mynewtasks=mynewtasks.filter(m => m.id !== id);                           // filtering all tasks and saving it in mynewtasks
+            localStorage.setItem("mytasks",JSON.stringify(mynewtasks));               //adds the remaining task cards to local storage and giving it name mytasks
+        }    
+                      
                              
-        displayListHtml(edifunc,delfunc){
+        // displayListHtml(edifunc,delfunc){
               
-                // alert("I am in display");
-                this.master.innerHTML ="";
-                    for (let i=0 ; i< this.cardArr.length ; i++){
-                        const nCard = new Card (this.cardArr[i].id,
-                            this.cardArr[i].cname,
-                            this.cardArr[i].description,
-                            this.cardArr[i].assignee,
-                            this.cardArr[i].dDate,
-                            this.cardArr[i].st
-                        );
-                    let taskElement = nCard.toElement(edifunc,delfunc);
-                    this.master.append(taskElement);
-                    }
-            }  
+        //         // alert("I am in display");
+        //         this.master.innerHTML ="";
+        //             for (let i=0 ; i< this.cardArr.length ; i++){
+        //                 const nCard = new Card (this.cardArr[i].id,
+        //                     this.cardArr[i].cname,
+        //                     this.cardArr[i].description,
+        //                     this.cardArr[i].assignee,
+        //                     this.cardArr[i].dDate,
+        //                     this.cardArr[i].st
+        //                 );
+        //             let taskElement = nCard.toElement(edifunc,delfunc);
+        //             this.master.append(taskElement);
+        //             }
+        //     }  
+        displayTask(){
+            this.master.innerHTML ="";                                      //refreshing page content 
+            let mynewtasks= JSON.parse(localStorage.getItem ("mytasks"));   
+            this.cardArr= mynewtasks; 
             
-            displayFromstorage(edifunc,delfunc)
-            {       this.master.diplayHTML ="";
-                   let mynewtasks= JSON.parse(localStorage.getItem("mytasks")) ;
-                  
-                  if (mynewtasks){
-                   for (let i=0 ; i< mynewtasks.length ; i++){
-                       const nCard = new Card (mynewtasks[i].id,
-                           mynewtasks[i].cname,
-                           mynewtasks[i].description,
-                           mynewtasks[i].assignee,
-                           mynewtasks[i].dDate,
-                           mynewtasks[i].st
-                       );
-                       let taskElement = nCard.toElement(edifunc,delfunc);
-                       this.master.append(taskElement);
-                     }
-                   }
-               }
+            for (let i=0 ; i< mynewtasks.length ; i++){
+                const nCard = new Card (mynewtasks[i].id,
+                    mynewtasks[i].cname,
+                    mynewtasks[i].description,
+                    mynewtasks[i].assignee,
+                    mynewtasks[i].dDate,
+                    mynewtasks[i].st
+                );
+            let taskElement = nCard.toElement();
+            this.master.append(taskElement);
+        
+                }
+                
+            } 
+            
              
 
 
